f05c844160862719eeb825ba8cdd1662785ed8f7
Ricky Moino <rickymafra@gmail.com>
2025-01-06 20:45:58 -0500

    Refactor and make some UI adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,11 @@ export default function Home() {
     <main className="flex min-h-screen w-full items-center justify-center">
       <div className="m-4 h-[--screen-h] w-full md:m-0 md:w-[--screen-w]">
         <section
-          className="glass-card items-center justify-between"
+          className="glass fixed left-0 top-0 z-10 flex w-full items-center justify-between space-x-4 border-b p-4 shadow-sm md:left-auto md:top-4 md:w-[--screen-w] md:rounded-md md:p-6"
           data-testid="header-section"
         >
           <div className="flex items-center justify-center space-x-4">
-            <MyBeaconLogo className="size-12" />
+            <MyBeaconLogo className="-mb-1 size-12" />
             <div className="flex-col">
               <h1 className="text-lg font-bold md:text-2xl">{t("title")}</h1>
               <h2 className="text-xs font-medium text-gray-500 dark:text-gray-300 md:text-lg">
